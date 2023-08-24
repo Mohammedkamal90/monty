7f14a0b52387fda5b8ff52ad2cdcd909363f9e52
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-08-24 05:41:09 -0700

    files

---
+++ rotr.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+  *x_rotr- Rotat stack to bottom
+  *@head:Stack
+  *@counter: Line Number
+  *return: NoN return
+ */
+
+void x_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
+{
+	stack_t *copy;
+
+	copy = *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	while (copy->next)
+	{
+		copy = copy->next;
+	}
+	copy->next = *head;
+	copy->prev->next = NULL;
+	copy->prev = NULL;
+	(*head)->prev = copy;
+	(*head) = copy;
+}
