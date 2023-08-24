3bbb0daaf1daf0ef44264711845e64bdac462de3
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-08-24 05:08:31 -0700

    stack

---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * x_stack - Print top
+ * @head: Stack
+ * @counter: Line_number
+ * return: NON return
+*/
+
+void x_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
