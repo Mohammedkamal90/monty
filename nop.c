29c55d19a8df581a1069e1643f767a5c7f9d407d
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-08-26 13:10:51 -0700

    all

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+  *x_nop- N0Thing
+  *@ head: st head
+  *@counter: line Number
+  *return: N0N Return
+ */
+
+void x_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
---
+++ rotl.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+  *x_rotl- Rotate stack of top
+  *@head: Stack
+  *@counter: line number
+  *return: N0N Return
+ */
+
+void x_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
+{
+	stack_t *tmp = *head, *aux;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	aux = (*head)->next;
+	aux->prev = NULL;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = *head;
+	(*head)->next = NULL;
+	(*head)->prev = tmp;
+	(*head) = aux;
+}
